d5662ade8ce44081321d78c88feffdbdb3c2042a
ueong <dnfod@me.com>
2021-04-16 17:34:20 +0900

    practice 3 start

---
+++ checkoutservice/src/main/java/com/samsungsds/checkoutservice/shipping/ShippingRequest.java
@@ -0,0 +1,38 @@
+package com.samsungsds.checkoutservice.shipping;
+
+import java.util.Arrays;
+import com.samsungsds.checkoutservice.cart.CartItem;
+
+public class ShippingRequest {
+    private CartItem[] cartItems;
+    private Address shippingAddress;
+
+    public ShippingRequest(CartItem[] cartItems, Address shippingAddress) {
+        this.cartItems = cartItems;
+        this.shippingAddress = shippingAddress;
+    }
+
+    public CartItem[] getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(CartItem[] cartItems) {
+        this.cartItems = cartItems;
+    }
+
+    public Address getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(Address shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "ShippingRequest{" +
+                "cartItems=" + Arrays.toString(cartItems) +
+                ", shippingAddress=" + shippingAddress +
+                '}';
+    }
+}
