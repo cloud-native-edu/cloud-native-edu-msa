b997080e70b951e4b884fb7e25490cc437d3e09e
ueong <dnfod@me.com>
2021-04-21 11:01:26 +0900

    cache complete

--- recommendservice/index.js
+++ recommendservice/index.js
@@ -3,16 +3,37 @@ const axios = require("axios");
 const _ = require("lodash");
 const app = express();
 const dotenv = require("dotenv");
+const { setupCache } = require('axios-cache-adapter');
 dotenv.config();
 
 const port = process.env.PORT || 8093;
-const urlProductService =
-  process.env.URL_PRODUCTSERVICE || "http://localhost:8092";
+const urlProductService = process.env.URL_PRODUCTSERVICE || "http://localhost:8092";
+
+//axios instance with cache
+const cache = setupCache({
+	maxAge: 2 * 60 * 1000 // 2 min
+})
+
+const axiosWithCache = axios.create({
+	adapter: cache.adapter
+})
+
+// axios configurations for measure request time
+axiosWithCache.interceptors.request.use( x => {
+	x.meta = x.meta || {}
+	x.meta.requestStartedAt = new Date().getTime()
+	return x
+})
+
+axiosWithCache.interceptors.response.use(x => {
+	console.log(`Execution time for: ${x.config.url} - ${new Date().getTime() - x.config.meta.requestStartedAt} ms`)
+    return x
+})
 
 app.get("/api/recommends", async (req, res) => {
   console.log("Recommendation List");
   try {
-    const response = await axios.get(urlProductService + "/api/products");
+    const response = await axiosWithCache.get(urlProductService + "/api/products");
     const responseData = JSON.stringify({
       recommendations: _.sampleSize(response.data.products, 4),
     });
